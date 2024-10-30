7c2e823bdb73211f45b6d232fe09bfc18251d04c
boxao555 <hongq1905@gmail.com>
2024-10-30 22:31:13 +0700

    Fix API Request Camera

--- Server/CameraServer/CameraServer/Controllers/LoginCameraController.cs
+++ Server/CameraServer/CameraServer/Controllers/LoginCameraController.cs
@@ -16,13 +16,16 @@ namespace CameraServer.Controllers
         }
 
         [HttpPost("login")] 
-        public IActionResult LoginCamera([FromBody] Camera camera) {
-           
+        public IActionResult LoginCamera(string cameraName, string password) {
+
+            Camera camera = new Camera
+            {
+                CameraName = cameraName,
+                Password = password
+            };
+
             var checkCam = _context.Cameras.Any(cam => cam.CameraName == camera.CameraName);
             if (!checkCam) { return BadRequest("Tên đăng nhập Camera không chính xác!."); }
-
-            var checkCode = _context.Cameras.Any(code => code.ConnectionCode == camera.ConnectionCode);
-            if (!checkCode) { return BadRequest("Code kết nối không chính xác!"); }
 
             var checkPassWord = _context.Cameras.Any(pass => pass.Password == camera.Password);
             if (!checkPassWord) { return BadRequest("Mật khẩu không trùng khớp!"); }
--- Server/CameraServer/CameraServer/obj/Debug/net8.0/CameraServer.AssemblyInfo.cs
+++ Server/CameraServer/CameraServer/obj/Debug/net8.0/CameraServer.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CameraServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb9b71d15a81cbbc89793ce5925a93bcf5bcaf3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcf9b59daa60948a2e6e37e239ec12ec6e106a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("CameraServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CameraServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
